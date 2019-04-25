315ef3aaa21682c28c48a446bafb833cf604dfb8
sawyerRick <33996345+sawyerRick@users.noreply.github.com>
2019-04-25 11:31:52 +0800

    Add files via upload

---
+++ view.sql
@@ -0,0 +1,39 @@
+create   database test
+go
+use test
+go
+
+create  table  s(
+sno   char(2),
+sn char(8),
+sex char(2),
+age tinyint,
+dept char(10)
+)
+go
+create  table   t
+(
+tno   char(2),
+tn    char(8),
+sex  char(2),
+age  tinyint,
+prof   char(10),
+sal smallint,
+comm   smallint,
+dept  char(10)
+
+)
+go
+create     table  c 
+(
+cno   char(2),
+cn   char(10),
+ct  tinyint
+)
+go
+create  table sc
+(
+sno    char(2),
+cno   char(2),
+score   tinyint
+)
